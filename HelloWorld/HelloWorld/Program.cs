2b29348f6d8b38f16a9db7be97b2ba5e463d9650
oscarjakobsson <oscar.jakobsson@lexicon.se>
2015-07-29 15:23:56 +0200

    Stärkte svaret en aning

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -13,7 +13,7 @@ namespace HelloWorld
         {
             Console.WriteLine("What is your name!?");
             string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name + "!");
+            Console.WriteLine("Hello " + name + "!?"); // Konsekvent tonläge
             Console.ReadKey();
         }
     }
